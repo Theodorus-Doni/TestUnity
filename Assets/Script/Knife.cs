b70867e40f6ae9d20892cd7fbff40a2b678cf752
Theodorus Doni <theodorusdoni4@gmail.com>
2021-04-08 07:24:25 +0700

    Coba

---
+++ Assets/Script/Knife.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour {
+
+    //false stop move
+    bool boolCheckMove;
+
+    private Rigidbody2D rigidThis;
+
+    bool checkColl;
+
+    void Start () {
+        rigidThis = GetComponent<Rigidbody2D>();
+        boolCheckMove = true;
+        transform.right = new Vector3(0,0,0) - transform.position;
+	}
+
+    private void FixedUpdate()
+    {
+        if (boolCheckMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 0, 0), 5 * Time.deltaTime);
+        }
+        
+    }
+
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Touch") && !checkColl)
+        {
+            checkColl = true;
+            StartCoroutine(Wait());
+        }
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(0.01f);
+        rigidThis.AddForce(Slice.vec2Direction * 10f,ForceMode2D.Force );
+        rigidThis.AddTorque(500, ForceMode2D.Force);
+        boolCheckMove = false;
+    }
+}
+
---
+++ Assets/Script/Slice.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slice : MonoBehaviour {
+
+    [SerializeField]
+    private Transform gameobjSlice = null;
+
+    [SerializeField]
+    private ParticleSystem particleSlice = null;
+
+    public static Vector2 vec2Direction;
+    public static float floatDistace;
+
+
+
+    private Vector2 vec2StartPos,vec2EndPos;
+
+    private Vector2 screenPoint;
+    private Vector2 offset;
+
+
+    float floatCheckMove;
+
+    void FixedUpdate () {
+        //mouse input
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 touchPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            particleSlice.Play();
+            vec2StartPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector2 cursorPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            cursorPoint = Camera.main.ScreenToWorldPoint(cursorPoint);
+            gameobjSlice.position = cursorPoint;
+            if (floatCheckMove < 5)
+            {
+                floatCheckMove += 1 * Time.deltaTime;
+            }
+            else if (floatCheckMove >= 5 ) {
+                
+            }
+
+
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            particleSlice.Stop();
+        }
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Knife") )
+        {
+            vec2EndPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            floatDistace = Vector2.Distance(vec2StartPos, vec2EndPos);
+            vec2Direction = vec2EndPos - vec2StartPos;
+        }
+    }
+}
